2e06667263d712472de380757fadd421660a9670
nukehistorylab <mjc96@columbia.edu>
2014-06-09 13:03:33 -0400

    Learned a bit about functions

---
+++ earthquake_code.py
@@ -0,0 +1,15 @@
+
+def greeting():
+	name = raw_input("what is your name? ")
+	print "Hello, " + name
+
+def hello(name):
+	print "Hello, " + name
+	
+def add (a, b):
+	return a+b
+	
+	hello("Soma")
+
+print add(2, 3) + add(4, 5)
+
